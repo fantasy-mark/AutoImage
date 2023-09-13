e85a5c9767e51c69de0d0a9da2bb1f043e1dc720
mark <hacker.do@163.com>
2023-09-13 16:02:26 +0800

    Build image at https://github.com/fantasy-mark/AutoImage/actions

--- run.sh
+++ run.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
 git add .
-first_line=$(head -n 1 Dockerfile)
-pattern="# ([^ ]+)"
-if [[ $first_line =~ $pattern ]]; then
-	image_name=${BASH_REMATCH[1]}
-fi
-echo "Build $image_name to https://github.com/fantasy-mark/AutoImage/actions"
-git commit -m "Build $image_name to https://github.com/fantasy-mark/AutoImage/actions"
+git commit -m "Build image at https://github.com/fantasy-mark/AutoImage/actions"
 git push -u origin main
